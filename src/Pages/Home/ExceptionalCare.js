d9573b964cb67187fb4850045d0702937f77cdfa
ahammedmd852 <ahammedmd852@gmail.com>
2022-05-18 22:44:38 +0600

    daypicker framwork added

--- src/Pages/Home/ExceptionalCare.js
+++ src/Pages/Home/ExceptionalCare.js
@@ -6,9 +6,9 @@ const ExceptionalCare = () => {
     return (
         <div className="hero min-h-screen my-20">
             <div className="hero-content flex-col lg:flex-row">
-                <img width={'455px'} height={'575px'} className='p-12' src={treatment} />
+                <img width={'455px'} height={'575px'} className='p-12' src={treatment}  alt=''/>
                 <div>
-                    <h1 className="text-5xl font-bold">Exceptional Dental Care, on Your Terms</h1>
+                    <h1 className="text-5xl font-bold font-serif">Exceptional Dental Care, on Your Terms</h1>
                     <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                     <PrimaryBtn>Get Started</PrimaryBtn>
                 </div>
